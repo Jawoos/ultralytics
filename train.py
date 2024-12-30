dbb62119bc3d9f33283bb567f9e7deb5d1851a4c
jhlee15 <jhlee15@github.com>
2024-12-30 16:28:29 +0900

    fix loss

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ def main(args):
     # Train the model
     train_results = model.train(
         # data="coco8.yaml",  # path to dataset YAML
-        # data="/DATA_17/DATASET/Competition_Dataset/CytologIA/images/temp/data.yaml",
-        data=args.data_path,
+        data="/workspace/data_path/DATASET/Competition_Dataset/CytologIA/yolo/data.yaml",
+        # data=args.data_path,
         epochs=int(args.epoch),  # number of training epochs
         imgsz=320,  # training image size
         device=args.gpu_num,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
@@ -34,7 +34,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoch', default=100, help='Epoch for Train')
-    parser.add_argument('--gpu_num', default='3', type=str, nargs='+', help='0 1 ...')  # 공백으로 리스트 구현
+    parser.add_argument('--gpu_num', default='0', type=str, nargs='+', help='0 1 ...')  # 공백으로 리스트 구현
     parser.add_argument('--batch_size', default=1, help='Batch Size for Train')
     parser.add_argument('--data_path', default='coco8.yaml', help='Data for train')
     args = parser.parse_args()
