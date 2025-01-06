5e0252134f7e30ccee4e3450a22b4f02cee5a8aa
jawoos <jaewoo1218@yonsei.ac.kr>
2025-01-06 19:44:27 +0900

    [dev] delet batch * 2 in multi gpu

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ warnings.filterwarnings("ignore")
 def main(args):
     # Load a model
     # model = YOLO("yolo11x.pt")
-    # model = XiiYOLO("yolo11x.pt")
-    model = XiiYOLO("/DATA_17/pjw/workspace/ultralytics/runs/detect/train45/weights/last.pt")
+    model = XiiYOLO("yolo11x.pt")
+    # model = XiiYOLO("/DATA_17/pjw/workspace/ultralytics/runs/detect/train45/weights/last.pt")
 
     # Train the model
     train_results = model.train(
@@ -42,7 +42,7 @@ def main(args):
         save_period=5,
         batch=args.batch_size,
         project = "./runs/detect/",
-        resume = True
+        # resume = True
     )
 
     # model = XiiYOLO("/DATA_17/pjw/workspace/ultralytics/runs/detect/train/weights/best.pt")
@@ -61,9 +61,9 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', default=4, help='Batch Size for Train')
     # parser.add_argument('--data_path', default='coco8.yaml', help='Data for train')
     # parser.add_argument('--data_path', default='/DATA1/temp/data_tiny_balanced.yaml', help='Data for train')
-    # parser.add_argument('--data_path', default='/DATA/DATASETS/temp/data_tiny_balanced.yaml', help='Data for train')
+    parser.add_argument('--data_path', default='/DATA/DATASETS/temp/data_tiny_balanced.yaml', help='Data for train')
 
-    parser.add_argument('--data_path', default='/DATA/DATASETS/temp/data_all.yaml', help='Data for train')
+    # parser.add_argument('--data_path', default='/DATA/DATASETS/temp/data_all.yaml', help='Data for train')
 
     args = parser.parse_args()
 
--- ultralytics/xiilab/engine/trainer.py
+++ ultralytics/xiilab/engine/trainer.py
@@ -283,12 +283,18 @@ class BaseTrainer:
 
         # Dataloaders
         batch_size = self.batch_size // max(world_size, 1)
+
         self.train_loader = self.get_dataloader(self.trainset, batch_size=batch_size, rank=LOCAL_RANK, mode="train")
         if RANK in {-1, 0}:
+            if RANK == 0:   # Multi GPU
             # Note: When training DOTA dataset, double batch size could get OOM on images with >2000 objects.
-            self.test_loader = self.get_dataloader(
-                self.testset, batch_size=batch_size if self.args.task == "obb" else batch_size * 2, rank=-1, mode="val"
-            )
+                self.test_loader = self.get_dataloader(
+                    self.testset, batch_size=batch_size if self.args.task == "obb" else batch_size, rank=-1, mode="val"
+                )
+            elif RANK == -1:    # Single GPU
+                self.test_loader = self.get_dataloader(
+                    self.testset, batch_size=batch_size if self.args.task == "obb" else batch_size * 2, rank=-1, mode="val"
+                )
             self.validator = self.get_validator()
             metric_keys = self.validator.metrics.keys + self.label_loss_items(prefix="val")
             self.metrics = dict(zip(metric_keys, [0] * len(metric_keys)))
